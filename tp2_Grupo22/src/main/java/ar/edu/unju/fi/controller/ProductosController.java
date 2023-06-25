bd1cff4a1c1a80bfa5dd306e3adae003999ab4d4
JoaquinNB31 <burgosjoaquin415@gmail.com>
2023-06-25 19:39:56 -0300

    JOAQUIN BURGOS| CRUD de página Productos funcional con MySQL
    ATENCION: Commit de Joaquin Burgos, el me pasó su código debido a cortes
    de internet que tuvo, por lo que no pudo subir a GitHub por el mismo sus
    commits.
    IProductoRepository.java añadido. Ahora las operaciones de Añadir,
    Modificar y Eliminar funcionan sobre la base de datos MySQL, siendo
    eliminar reemplazado por una eliminación lógica, basandose en si la
    propiedad estado es verdadero o falso. Falso es eliminado (no aparece en
    la lista).
    Arreglado el error en el que aparece un error en inglés.
    productos.html ahora tiene un selector de categoría, lo que filtra por
    categoría la lista de productos

--- tp2_Grupo22/src/main/java/ar/edu/unju/fi/controller/ProductosController.java
+++ tp2_Grupo22/src/main/java/ar/edu/unju/fi/controller/ProductosController.java
@@ -1,11 +1,13 @@
 package ar.edu.unju.fi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.*;
 
+import ar.edu.unju.fi.entity.Consejo;
 import ar.edu.unju.fi.entity.Producto;
 import ar.edu.unju.fi.service.ICommonService;
 import ar.edu.unju.fi.service.IProductoService;
@@ -24,8 +26,9 @@ public class ProductosController {
 	
 	//Pagina con el listado de los productgos 
 @GetMapping("/listado")
-public String getProductosPage(Model model) {
-	model.addAttribute("productos", productoService.getListaP());
+public String getProductosPage(Model model, @RequestParam (name = "categoriaP", required = false) String categoriaP) {
+	model.addAttribute("productos", productoService.getListaP(categoriaP));
+	model.addAttribute("categoriaP", categoriaP);
 	return "productos";
 }
 
@@ -42,10 +45,10 @@ public String getNuevoProductoPage(Model model) {
 }
 
 //Página para modificar un producto
-@GetMapping("/modificar/{codigo}")
-public String getModificarProductosPage(Model model, @PathVariable(value = "codigo") int cod) {
+@GetMapping("/modificar/{id}")
+public String getModificarProductosPage(Model model, @PathVariable(value = "id") Long id) {
   boolean edicion = true;
-  Producto productoEncontrado = productoService.getBy(cod);
+  Producto productoEncontrado = productoService.getBy(id);
   if (productoEncontrado == null) {
       return "redirect:/productos/listado";
   }
@@ -57,9 +60,19 @@ public String getModificarProductosPage(Model model, @PathVariable(value = "codi
 
 
 @PostMapping("/modificar")
-public String modificarProducto(@ModelAttribute("producto") Producto producto) {
-    productoService.modificarProducto(producto);
-    return "redirect:/productos/listado";
+public ModelAndView getModificarProductoPage(@Valid @ModelAttribute("producto")Producto producto, BindingResult result) {
+	ModelAndView modelView = new ModelAndView("productos");
+	boolean edicion = true;
+	if(result.hasErrors()) {
+		modelView.setViewName("nuevo_producto");
+		modelView.addObject("producto", producto);
+		modelView.addObject("categorias", commonService.getProductoCategoria());
+		modelView.addObject("edicion", edicion);
+		return modelView;
+	}
+	productoService.guardarProducto(producto);
+//	modelView.addObject("consejos", productoService.getListaP());
+	return modelView;
 }
 
 //Método para guardar o modificar un producto
@@ -72,21 +85,16 @@ public ModelAndView guardarProducto(@Valid @ModelAttribute("producto") Producto 
       modelView.addObject("categorias", commonService.getProductoCategoria());
       return modelView;
   }
-
-  if (producto.getCodigo() != 0) { // Verifica si el código del producto es diferente de cero (indicando que es un producto existente)
-      productoService.modificarProducto(producto);
-  } else {
       productoService.guardarProducto(producto);
-  }
-
-  modelView.addObject("productos", productoService.getListaP());
+//  modelView.addObject("productos", productoService.getListaP());
   return modelView;
 }
 
 //Página para eliminar un producto
-@GetMapping("/eliminar/{codigo}")
-public String eliminarProducto(@PathVariable(value = "codigo") int cod) {
-    productoService.eliminarProducto(cod);
+@GetMapping("/eliminar/{id}")
+public String eliminarProducto(@PathVariable(value = "id") Long id) {
+	Producto productoEncontrado = productoService.getBy(id);
+    productoService.eliminarProducto(productoEncontrado);
     return "redirect:/productos/listado";
 }
 }
--- tp2_Grupo22/src/main/java/ar/edu/unju/fi/service/IProductoService.java
+++ tp2_Grupo22/src/main/java/ar/edu/unju/fi/service/IProductoService.java
@@ -7,20 +7,20 @@ import jakarta.validation.Valid;
 
 public interface IProductoService {
 	
-	//Listar productos
-    List<Producto> getListaP();
-    //Guardar productos
-    void guardarProducto(@Valid Producto producto);
-    
-    //Obtener productos segun el codigo
-    	Producto getBy (int codigo);
-    
-    //Modificar un producto
-    void modificarProducto(Producto producto);
-    //Eliminar un producto
-    Producto eliminarProducto(int codigo);
+	// Listar productos
+    List<Producto> getListaP(String categoriaP);
 
-    //Obtener producto nuevo
+    // Guardar productos
+    void guardarProducto(Producto producto);
+
+    // Obtener producto por código
+    public Producto getBy(Long id);
+
+    // Eliminar un producto
+    void eliminarProducto(Producto productoEncontrado);
+
+    // Obtener producto nuevo
     Producto getProducto();
+
 
 }
